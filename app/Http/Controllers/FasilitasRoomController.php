96dd4684225bd3053493845b479e5571bc2a7801
Herry Prasetyo <herry081288@gmail.com>
2022-02-22 09:59:33 +0700

    first commit

---
+++ app/Http/Controllers/FasilitasRoomController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FasilitasRoom;
+use Illuminate\Http\Request;
+
+class FasilitasRoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FasilitasRoom  $fasilitasRoom
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FasilitasRoom $fasilitasRoom)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FasilitasRoom  $fasilitasRoom
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FasilitasRoom $fasilitasRoom)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FasilitasRoom  $fasilitasRoom
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FasilitasRoom $fasilitasRoom)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FasilitasRoom  $fasilitasRoom
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FasilitasRoom $fasilitasRoom)
+    {
+        //
+    }
+}
