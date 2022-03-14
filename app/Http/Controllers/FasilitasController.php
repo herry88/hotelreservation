cdb6598a4d2edffa2eeb08a9a388faf3479e5352
Herry Prasetyo <herry081288@gmail.com>
2022-03-14 10:18:06 +0700

    fasilitasDone

--- app/Http/Controllers/FasilitasController.php
+++ app/Http/Controllers/FasilitasController.php
@@ -15,6 +15,7 @@ class FasilitasController extends Controller
     public function index()
     {
         //
+        return view('admin.master');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//rute fasilitas
+Route::resource('fasilitas', App\Http\Controllers\FasilitasController::class);
